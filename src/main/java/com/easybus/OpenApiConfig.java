319a974efca99019da0abe362d25f11cc08800e0
Bharath <varamasalva8@gmail.com>
2025-08-21 22:31:23 +0530

    openAPI

--- src/main/java/com/easybus/OpenApiConfig.java
+++ src/main/java/com/easybus/OpenApiConfig.java
@@ -1,20 +1,20 @@
-//package com.easybus;
-//
-//
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//
-//import io.swagger.v3.oas.models.OpenAPI;
-//import io.swagger.v3.oas.models.info.Info;
-//
-//@Configuration
-//public class OpenApiConfig {
-//    @Bean
-//    public OpenAPI apiInfo() {
-//        return new OpenAPI().info(new Info()
-//                .title("User API")
-//                .description("User management endpoints")
-//                .version("v1"));
-//    }
-//    
-//}
+package com.easybus;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+
+@Configuration
+public class OpenApiConfig {
+    @Bean
+    public OpenAPI apiInfo() {
+        return new OpenAPI().info(new Info()
+                .title("User API")
+                .description("User management endpoints")
+                .version("v1"));
+    }
+    
+}
